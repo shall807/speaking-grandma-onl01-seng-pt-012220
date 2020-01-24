3b9b33f91a785b9c6adf6e8b2076da12c8567abe
Stevi Hall <career.sh.work@gmail.com>
2020-01-24 23:26:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-if speak_to_grandma == "I LOVE YOU GRANDMA!"
+if speak_to_grandma == capitals
   puts "I LOVE YOU TOO PUMPKIN!"
 elseif puts "HUH?! SPEAK UP, SONNY!"
 
